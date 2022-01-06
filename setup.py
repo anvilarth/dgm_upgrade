6332325723b06410191cf6af380ed8a1d0291ca8
anvilarth <43551010+anvilarth@users.noreply.github.com>
2022-01-06 10:42:55 +0300

    small fixes

--- setup.py
+++ setup.py
@@ -5,5 +5,18 @@ setup(
     name='dgm_utils',
     version='0.1.0',
     packages=find_packages(),
+    install_requires=[
+        'numpy', 
+        'matplotlib',
+        'pillow',
+        'pickle',
+        'tqdm',
+        'torch',
+        'torchvision', 
+        'torchaudio',
+        'gdown',
+        'scikit-learn',
+        'wandb',
+    ]
     license='MIT License',
 )
